a0336ad27af652c0206e31e6e36e94a84c3bc63c
Dhruv Tyagi <dhruv.tya@gmail.com>
2020-11-02 05:10:06 -0800

    Added audio client node

---
+++ aud_com_nav/src/aud_client.cpp
@@ -0,0 +1,43 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include "aud_com_nav/GoalFromAudio.h"
+
+ros::ServiceClient aud_client;
+
+void audio_callback(const std_msgs::String aud_msg){
+  ROS_INFO("Audio command heard : %s", aud_msg);
+  aud_com_nav::GoalFromAudio aud_srv;
+ 
+  if(aud_msg=="kitchen")
+     aud_srv.request.audio_goal = 1;
+  else if(aud_msg=="hall")
+     aud_srv.request.audio_goal = 2;
+  else if(aud_msg=="room one")
+     aud_srv.request.audio_goal = 3;
+  else if(aud_msg=="empty two")
+     aud_srv.request.audio_goal = 5;
+  else{
+     ROS_INFO("Going to default hall position");
+     aud_srv.request.audio_goal = 2;
+  }
+
+
+  if(!aud_client.call(aud_srv))
+        ROS_ERROR("Failed to call service safe_move");
+
+}
+
+int main(int argc,char** argv){
+
+  ros::init(argc, argv, "aud_client_node");
+
+  ros::NodeHandle n;
+
+  aud_client = n.serviceClient<aud_com_nav::GoalFromAudio>("/aud_com_nav/goal_service");
+
+  ros::Subscriber aud_sub = n.subscribe("/output",1,audio_callback);
+
+  ros::spin();
+
+  return 0;
+}
